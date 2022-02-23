10a54073f86a745bcfd277e5c4463a07d4166ffd
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-22 21:36:54 -0500

    Props definido para iteracción con más datos

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
 
- const UserTable = () => (
-    <table>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Username</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
+ const UserTable = (props) => {
+    console.log(props.users);
+    return (
+        <table>
+        <thead>
         <tr>
-            <td>Name data</td>
-            <td>Name data</td>
-            <td>
-                <button className='button muted-button'> Edit </button>
-                <button className='button muted-button'> Delete </button>
-            </td>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Actions</th>
         </tr>
-    </tbody>
-    
-  </table>
- );
-
+        </thead>
+        <tbody>
+            {
+                props.users.map(user =>(
+                    <tr>
+                        <td>Name data</td>
+                        <td>Username data</td>
+                        <td>
+                            <button className='button muted-button'> Edit </button>
+                            <button className='button muted-button'> Delete </button>
+                        </td>
+                    </tr> 
+                ))
+            }
+            
+        </tbody>    
+    </table>);
+}
 
 export default UserTable
