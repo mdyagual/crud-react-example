e39561f4e772b8c70374dff01e085262f07713bb
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-02-22 20:46:14 -0500

    Actualización acorde a la guía de replicación

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 function App() {
   return (
-    <div>Hola mundo~~~</div>
+    //<div>Hola mundo~~~</div>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">        
+            <h2>Add user</h2>
+          </div>
+          <div className="flex-large">
+            <h2>View users</h2>         
+          </div>
+      </div>
+    </div>
   );
 }
 
